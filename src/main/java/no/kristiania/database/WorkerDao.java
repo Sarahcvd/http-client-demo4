3dc1fc9f6adf4728c3dd1b8d028e30658ae52b56
Waligustav <wgbjork@gmail.com>
2020-10-13 17:58:18 +0200

    add and list information in Postgres

---
+++ src/main/java/no/kristiania/database/WorkerDao.java
@@ -0,0 +1,43 @@
+package no.kristiania.database;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class WorkerDao {
+
+    public static void main(String[] args) throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/kristianiaworker");
+        dataSource.setUser("kristianiashop");
+        dataSource.setPassword("harasilaw");
+
+        System.out.println("Please enter worker name:");
+        Scanner scanner = new Scanner(System.in);
+        String workerName = scanner.nextLine();
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO worker (full_name) VALUES (?)")) {
+                statement.setString(1, workerName);
+                statement.executeUpdate();
+
+            }
+        }
+
+
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from worker")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString("full_name"));
+                        System.out.println(rs.getString("email_address"));
+                    }
+                }
+            }
+        }
+    }
+}
